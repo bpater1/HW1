9950be77cb36ede5446a64a64e77de4a42b9b541
Brooklyn <bpater1@jh.edu>
2023-09-07 14:06:32 -0400

    ALL TESTS PASS

--- hw1q8.py
+++ hw1q8.py
@@ -1,7 +1,7 @@
 def find_most_frequent_substring(dna):
     substring_counts = {}
     max_count = 0
-    most_frequent_substrings = []
+    most_frequent_substring = ""
 
     for i in range(len(dna) - 5):
         substring = dna[i:i + 6]
@@ -12,11 +12,11 @@ def find_most_frequent_substring(dna):
 
         if substring_counts[substring] > max_count:
             max_count = substring_counts[substring]
-            most_frequent_substrings = [i]
-        elif substring_counts[substring] == max_count:
-            most_frequent_substrings.append(i)
+            most_frequent_substring = substring
 
-    return most_frequent_substrings
+    indices = [i for i in range(len(dna)) if dna.startswith(most_frequent_substring, i)]
+
+    return indices
 
 if __name__ == "__main__":
     dna_sequence = input().strip()
