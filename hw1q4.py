6a419c37465b974a6320ad41beb6fff3a55f465b
Brooklyn <bpater1@jh.edu>
2023-09-07 13:06:27 -0400

    q4 test now passing

--- hw1q4.py
+++ hw1q4.py
@@ -2,32 +2,23 @@ def reverse_complement(dna):
     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
     return ''.join(complement[base] for base in dna[::-1])
 
+def generate_palindromes(n):
+    palindromes = set()
+    bases = 'ACGT'
 
-def generate_palindromes(sequence, n, length):
-    if length > n:
-        return
+    def generate(palindrome):
+        if len(palindrome) <= n:
+            rc = reverse_complement(palindrome)
+            if palindrome == rc:
+                palindromes.add(palindrome)
+            for base in bases:
+                generate(palindrome + base)
 
-    reverse_comp = reverse_complement(sequence)
-
-    if sequence == reverse_comp:
-        palindrome_set.add(sequence)  # Store unique palindromes
-
-    for base in "ACGT":
-        if base not in sequence:
-            generate_palindromes(sequence + base, n, length + 1)
-
-
-def find_palindromes(n):
-    for base in "ACGT":
-        generate_palindromes(base, n, 1)
-
+    generate('')
+    return sorted(palindromes)
 
 if __name__ == "__main__":
     n = int(input())
-    palindrome_set = set()
-    find_palindromes(n)
-
-    # Sort and print the palindromes in alphabetical order
-    palindromes = sorted(palindrome_set)
+    palindromes = generate_palindromes(n)
     for palindrome in palindromes:
         print(palindrome)
