0810ae3f608826bb76a45b94987c014b328ab6be
Brooklyn <bpater1@jh.edu>
2023-09-07 13:57:31 -0400

    Q3 fixed, Q8 Remains

--- hw1q3.py
+++ hw1q3.py
@@ -1,18 +1,19 @@
+import sys
 
 def transcription(dna):
     count = 1
     transcribed_dna = ""
     for base in dna:
-      if count % 3 == 0 and count != len(dna):
-         base+='-'
-      if base == 'T':
-        base = 'U'
-      transcribed_dna+= base
-      count += 1
+        if count % 3 == 0 and count != len(dna):
+            base += '-'
+        if base == 'T':
+            base = 'U'
+        transcribed_dna += base
+        count += 1
     return ''.join(transcribed_dna)
 
 if __name__ == "__main__":
-    dna_input = input()
+    dna_input = ''.join(sys.stdin.readlines())
     dna_cleaned = ''.join(base for base in dna_input if base in 'ACTG')
     transcribed = transcription(dna_cleaned)
     print(transcribed)
